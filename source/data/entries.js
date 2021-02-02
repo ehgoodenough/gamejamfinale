f230e89ccb0372495d538877b32d84dcd89efc0d
Andrew Goodenough <ehgoodenough@gmail.com>
2021-02-01 16:37:47 -0800

    Sanitized the inputs for valid youtube urls. (#2)

--- source/data/entries.js
+++ source/data/entries.js
@@ -5,6 +5,8 @@
 //     "youtube": "TGlgNDDWzgw", // the video id of a youtube video
 // }
 
+import {parseYoutube} from "library/parse.js"
+
 let entries = require("data/entries.csv")
 let headers = entries.shift()
 
@@ -22,12 +24,5 @@ entries.forEach((entry) => {
     entry.emoji = entry["Emoji"]
     entry.youtube = parseYoutube(entry["Youtube Link"])
 })
-
-// https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
-function parseYoutube(url) {
-    if(url == undefined) return undefined
-    const match = url.match(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/)
-    return (match && match[7].length == 11) ? match[7] : undefined
-}
 
 export default entries
